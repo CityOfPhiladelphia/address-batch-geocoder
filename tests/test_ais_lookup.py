d3f8f1868f67fe3e0415c76402acdab68255df04
CaitlinCP <caitlin.c.pratt@gmail.com>
2025-11-17 21:45:35 +0000

    Write tiebreaking unit tests

--- tests/test_ais_lookup.py
+++ tests/test_ais_lookup.py
@@ -59,6 +59,122 @@ def test_ais_lookup_creates_address_search_url(monkeypatch):
         "is_multiple_match": False
     }
 
+def test_ais_lookup_tiebreaks(monkeypatch):
+    created = {}
+
+    class FakeResponse:
+        def __init__(self, data, status_code=200):
+            self._data = data
+            self.status_code = status_code
+
+        def __getitem__(self, k):
+            return self._data[k]
+
+        def __bool__(self):
+            return True
+
+        def json(self):
+            return self._data
+
+    class FakeSession:
+        def get(self, *a, **k):
+            raise AssertionError("should be patched")
+
+    def fake_get(self, url, params=None, timeout=None, **kwargs):
+        created["url"] = url
+        created["params"] = params
+        return FakeResponse(
+            {
+                "features": [
+                    {
+                        "properties": {"street_address": "1234 N MARKET ST", "zip_code": "19107"},
+                        "geometry": {"coordinates": [-75.16, 39.95]},
+                    },
+                    {
+                        "properties": {"street_address": "1234 S MARKET ST", "zip_code": "11111"},
+                        "geometry": {"coordinates": [-75.16, 39.95]},
+                    }
+
+                ]
+            },
+            200,
+        )
+
+    monkeypatch.setattr(FakeSession, "get", fake_get)
+    sess = FakeSession()
+
+    result = ais_lookup.ais_lookup(sess, "1234", "1234 mkt st", "19107", [])
+
+    assert created["url"] == "https://api.phila.gov/ais/v1/search/1234 mkt st"
+    assert created["params"] == {"gatekeeperKey": "1234"}
+    assert result == {
+        "geocode_lat": "39.95",
+        "geocode_lon": "-75.16",
+        "is_addr": True,
+        "is_philly_addr": True,
+        "output_address": "1234 N MARKET ST",
+        "match_type": "ais",
+        "is_multiple_match": False
+    }
+
+def test_ais_lookup_returns_no_match_if_tiebreak_fails(monkeypatch):
+    created = {}
+
+    class FakeResponse:
+        def __init__(self, data, status_code=200):
+            self._data = data
+            self.status_code = status_code
+
+        def __getitem__(self, k):
+            return self._data[k]
+
+        def __bool__(self):
+            return True
+
+        def json(self):
+            return self._data
+
+    class FakeSession:
+        def get(self, *a, **k):
+            raise AssertionError("should be patched")
+
+    def fake_get(self, url, params=None, timeout=None, **kwargs):
+        created["url"] = url
+        created["params"] = params
+        return FakeResponse(
+            {
+                "features": [
+                    {
+                        "properties": {"street_address": "1234 N MARKET ST", "zip_code": "22222"},
+                        "geometry": {"coordinates": [-75.16, 39.95]},
+                    },
+                    {
+                        "properties": {"street_address": "1234 S MARKET ST", "zip_code": "11111"},
+                        "geometry": {"coordinates": [-75.16, 39.95]},
+                    }
+
+                ]
+            },
+            200,
+        )
+
+    monkeypatch.setattr(FakeSession, "get", fake_get)
+    sess = FakeSession()
+
+    result = ais_lookup.ais_lookup(sess, "1234", "1234 mkt st", "19107", [])
+
+    assert created["url"] == "https://api.phila.gov/ais/v1/search/1234 mkt st"
+    assert created["params"] == {"gatekeeperKey": "1234"}
+    assert result == {
+        "geocode_lat": None,
+        "geocode_lon": None,
+        "is_addr": False,
+        "is_philly_addr": False,
+        "output_address": None,
+        "match_type": "ais",
+        "is_multiple_match": True
+    }
+
 
 def test_false_address_returns_input_address_if_bad_address(monkeypatch):
 
